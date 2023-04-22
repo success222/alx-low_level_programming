e6b65ff58f60c01c9bc07273cbd971fba1dd64b4
success222 <sotonwasuccess1@gmail.com>
2023-04-22 09:53:59 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *description: print numbers from 0-9
+ *Return: always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}	
