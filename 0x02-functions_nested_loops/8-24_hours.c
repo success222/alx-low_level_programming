8b42f257d06b1b240b8a4c674b5c11c5ef1c4717
success222 <sotonwasuccess1@gmail.com>
2023-04-25 16:04:35 -0700

    jack Baeur

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *jack_baier - prints every minute of the day
+ *Return: nothing
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+			{
+				for (c = 0; c <= 5; c++)
+			}
+		}
+		
+	}
+}
