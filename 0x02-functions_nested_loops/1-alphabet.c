260ffbc3aca2af396cf7150e099e5dc90c6777fc
success222 <sotonwasuccess1@gmail.com>
2023-04-25 13:43:43 -0700

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet - prints alphabets
+ *description: print the alphabets jn lowercase
+ *Return: void
+ */
+void print_alphabet(void)
+{
+	char x;
+	
+	for(x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
+	
+
