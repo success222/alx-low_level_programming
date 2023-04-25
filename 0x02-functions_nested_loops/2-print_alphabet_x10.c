2a989bfa0e3d53b878b0118008414e6efca82cdc
success222 <sotonwasuccess1@gmail.com>
2023-04-25 14:03:03 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints the alphabets 10 times
+ *Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int y = 0;
+
+	while (y < 10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+		y++;
+	}
+}
